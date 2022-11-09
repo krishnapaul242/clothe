61c78ec6e382ddb1ac8a171108ae5741f909058d
Krishna Paul <krishnapaulmailbox@gmail.com>
2022-11-09 10:56:38 -0800

    boilerplate code

---
+++ src/Authentication/Onboarding/index.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+interface OnboardingProps { };
+
+const Onboarding = ({ }: OnboardingProps) => {
+    return (
+        <View style={{
+            flex: 1,
+            backgroundColor: 'cyan',
+        }}>
+        </View>
+    );
+};
+    
+export default Onboarding;
